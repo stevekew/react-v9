366c99ef58f0f4e3f27153d47254969e0189e729
Steve Kew <stephen.kew@gmail.com>
2025-12-28 17:32:56 +0100

    Added props to the Pizza component to make it customizable

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,7 +1,7 @@
-const Pizza = () => {
+const Pizza = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "The Pepperoni Pizza"),
-        React.createElement("p", {}, "Mozzarella Cheese, Pepperoni"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("p", {}, props.description),
     ]);
 };
 
@@ -9,9 +9,18 @@ const Pizza = () => {
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, "Pixel Perfect Pizzas"),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
+        React.createElement(Pizza, {
+            name: "The Pepperoni Pizza",
+            description: "Mozzarella Cheese, Pepperoni",
+        }),
+        React.createElement(Pizza, {
+            name: "The Hawaiian Pizza",
+            description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+        }),
+        React.createElement(Pizza, {
+            name: "The Big Meat Pizza",
+            description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+        }),
     ]);
 };
 
