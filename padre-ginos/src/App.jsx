cde1e2629b59cdc7341d214dc7a56ab49c45bb08
Stephen Kew <stephen.kew@gmail.com>
2026-01-04 22:16:05 +0100

    Updated App.js to use jsx

---
+++ padre-ginos/src/App.jsx
@@ -0,0 +1,17 @@
+ import { createRoot } from 'react-dom/client'
+import Pizza from './Pizza';
+
+const App = () => {
+  return (
+  <div>
+      <h1>Padre Gino's - Order Now</h1>
+      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
+      <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese" />
+      <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
