5f1444b1b965b04ecc857f582ee527dc0a0e3f00
Stephen Kew <stephen.kew@gmail.com>
2026-01-06 23:38:14 +0100

    Added static images to the Pizza component and a stylesheet to the page

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,13 +1,25 @@
- import { createRoot } from 'react-dom/client'
-import Pizza from './Pizza';
+import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
 
 const App = () => {
   return (
-  <div>
+    <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
-      <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese" />
-      <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" />
+      <Pizza
+        name="Pepperoni"
+        description="Mozzarella Cheese, Pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image={"/public/pizzas/big_meat.webp"}
+      />
     </div>
   );
 };
