155781cd99fbd44b6e7735cf406ba5311ddccb7b
Stephen Kew <stephen.kew@gmail.com>
2026-01-08 13:13:10 +0100

    Added state hooks to allow the pizza size and type to be changed

--- padre-ginos/src/Order.jsx
+++ padre-ginos/src/Order.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Pizza from "./Pizza";
 
 export default function Order() {
-  const pizzaType = "pepperoni";
-  const pizzaSize = "M";
+  // const pizzaType = "pepperoni";
+  // const pizzaSize = "M";
+
+  const [pizzaType, setPizzaType] = useState("pepperoni");
+  const [pizzaSize, setPizzaSize] = useState("M");
 
   return (
     <div className="order">
@@ -11,7 +15,11 @@ export default function Order() {
         <div>
           <div>
             <label htmlFor="pizza-type">Pizza Type</label>
-            <select name="pizza-type" value={pizzaType}>
+            <select
+              name="pizza-type"
+              value={pizzaType}
+              onChange={(e) => setPizzaType(e.target.value)}
+            >
               <option value="pepperoni">The Pepperoni Pizza</option>
               <option value="hawaiian">The Hawaiian Pizza</option>
               <option value="big_meat">The Big Meat Pizza</option>
@@ -23,6 +31,7 @@ export default function Order() {
               <span>
                 <input
                   checked={pizzaSize === "S"}
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   type="radio"
                   name="pizza-size"
                   value="S"
@@ -33,16 +42,18 @@ export default function Order() {
               <span>
                 <input
                   checked={pizzaSize === "M"}
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   type="radio"
                   name="pizza-size"
                   value="M"
                   id="pizza-m"
                 />
-                <label htmlFor="pizza-s">Medium</label>
+                <label htmlFor="pizza-m">Medium</label>
               </span>
               <span>
                 <input
                   checked={pizzaSize === "L"}
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   type="radio"
                   name="pizza-size"
                   value="L"
